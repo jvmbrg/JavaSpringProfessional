ed52254cd4a9e3f7e8bf6572409f54e6748c8f03
João Vítor Braga <jvitormbraga@gmail.com>
2026-02-24 12:40:08 -0300

    Add files via upload

---
+++ spring-professional/src/test/java/com/jvmbrg/spring_professional/SpringProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvmbrg.spring_professional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
